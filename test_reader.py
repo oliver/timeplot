e04b52942b4047081d5ce5976d2183a8f5e84f90
Oliver Gerlich <oliver.gerlich@gmx.de>
2011-04-03 13:13:38 +0200

    TestFuncReader now expects the function to be passed as string
    This allows to specify the function code in config file.

--- test_reader.py
+++ test_reader.py
@@ -1,21 +1,22 @@
 
 import time
+import math
 
 from base_reader import InputReader
 from event import EventMgr
 
 class TestFuncReader (InputReader):
-    def __init__ (self, sourceMgr, store, func, interval=100*1000, name="test"):
-        "func gets the current time as parameter and must return a value"
+    def __init__ (self, sourceMgr, store, function, interval=100*1000, name="test"):
         InputReader.__init__(self, store)
-        self.func = func
+
+        self.codeObj = compile(function, '<string>', 'eval')
         self.id = sourceMgr.register(name)
 
         EventMgr.startTimer(interval, self.onTimer)
 
     def onTimer (self):
         t = time.time()
-        value = self.func(t)
+        value = eval(self.codeObj)
         self.store.update( (self.id, t, value) )
         return True
 
