26b3023aeb594f72c82dc0b041885180c53b26e9
Oliver Gerlich <oliver.gerlich@gmx.de>
2012-07-25 22:25:12 +0200

    QtOutput: rework scrollbar to support larger/more fine-grained time ranges

--- qt_output.py
+++ qt_output.py
@@ -1,5 +1,6 @@
 
 import sys
+import math
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
@@ -7,6 +8,119 @@ from PyQt4 import QtCore
 from base_output import *
 
 from qt_output_ui import Ui_TimePlotWindow
+
+
+class QScrollbarLong:
+    "wrapper around QScrollbar, allowing to set a range > 2**32 (and also use float values)"
+    INTERNAL_MAX = 2**31
+
+    def __init__ (self, realScrollbar):
+        self.realScrollbar = realScrollbar
+        self.mini = None
+        self.maxi = None
+
+        self.internalRange = None
+
+        self._lastValue = 0
+
+    def _lengthToInternal (self, externalValue):
+        externalLength = self.maxi - self.mini
+        internalLength = self.internalRange
+
+        factor = float(internalLength) / externalLength
+        result = int(externalValue * factor)
+        assert result <= self.INTERNAL_MAX, "internal value (%d) must be < max (%d); factor: %f" % (
+            result, self.INTERNAL_MAX, factor)
+        assert isinstance(result, int), "internal value (%d) is not an int; factor: %f" % (
+            result, factor)
+        return result
+
+    def _lengthToExternal (self, externalValue):
+        externalLength = self.maxi - self.mini
+        internalLength = self.internalRange
+        factor = float(internalLength) / externalLength
+        return externalValue / factor
+
+    def _posToInternal (self, externalPos):
+        return self._lengthToInternal(externalPos - self.mini)
+
+    def _posToExternal (self, internalPos):
+        return self._lengthToExternal(internalPos) + self.mini
+
+
+    def setRange (self, minimum, maximum, pageStep, singleStep):
+        assert pageStep > 0
+        assert singleStep > 0
+
+        if self.mini != self.maxi:
+            self._lastValue = self._posToExternal(self.realScrollbar.value())
+
+        if maximum <= minimum:
+            # special case: no scrolling possible
+            self.mini = self.maxi = minimum
+            self.realScrollbar.setRange(0, 0)
+            self.realScrollbar.setPageStep(1)
+            self.realScrollbar.setSingleStep(1)
+            return
+
+        self.mini = minimum
+        self.maxi = maximum
+
+        length = maximum - minimum
+        assert length > 0
+        assert length < self.INTERNAL_MAX, "really large ranges not supported yet"
+
+        if pageStep > length:
+            pageStep = length
+        if singleStep > length:
+            singleStep = length
+
+        assert pageStep <= length
+        assert singleStep <= pageStep
+
+        # use smaller (finer-grained) steps internally, to allow finer-grained scrollbar sliding:
+        numMaxSteps = self.INTERNAL_MAX
+        numNecessarySteps = float(length) / singleStep
+
+        if numNecessarySteps > numMaxSteps:
+            assert False, "too many steps necessary (%f)" % numNecessarySteps
+        else:
+            stepFactor = int(numMaxSteps / numNecessarySteps)
+            assert stepFactor >= 1
+            numSteps = int(numNecessarySteps * stepFactor)
+            assert numSteps <= numMaxSteps, "numSteps (%d) must be less or equal to numMaxSteps (%d); numNecessarySteps=%d, stepFactor=%d" % (
+                numSteps, numMaxSteps, numNecessarySteps, stepFactor)
+            assert numSteps >= numNecessarySteps
+
+        assert numSteps < self.INTERNAL_MAX, "really small steps not supported yet"
+
+        self.internalRange = numSteps
+
+        self.realScrollbar.setRange(0, self.internalRange)
+        self.realScrollbar.setPageStep( self._lengthToInternal(pageStep) )
+        self.realScrollbar.setSingleStep( self._lengthToInternal(singleStep) )
+
+        if self._lastValue is not None:
+            self.setValue(self._lastValue)
+
+    def setValue (self, value):
+        if value < self.mini:
+            value = self.mini
+        elif value > self.maxi:
+            value = self.maxi
+        self._lastValue = value
+        if not(self.mini == self.maxi):
+            internalValue = self._posToInternal(value)
+            self.realScrollbar.setValue(internalValue)
+
+    def value (self):
+        if self.mini == self.maxi:
+            return self.mini
+        else:
+            return self._posToExternal(self.realScrollbar.value())
+
+    def isAtMax (self):
+        return (self.realScrollbar.value() == self.realScrollbar.maximum())
 
 
 class MainWindow (QtGui.QMainWindow, Ui_TimePlotWindow):
@@ -31,21 +145,23 @@ class QtOutput(BaseOutput):
         self.win = MainWindow()
         self.win.plotter.init(store)
 
+        self.hScroll = QScrollbarLong(self.win.hscrollPlotter)
+
         self.win.connect(self.win.actionZoomIn, QtCore.SIGNAL('activated()'), lambda: self.onZoom(2))
         self.win.connect(self.win.actionZoomOut, QtCore.SIGNAL('activated()'), lambda: self.onZoom(0.5))
-        self.win.hscrollPlotter.connect(self.win.hscrollPlotter, QtCore.SIGNAL('valueChanged(int)'), lambda val: self.updateSlider())
+        self.win.hscrollPlotter.connect(self.win.hscrollPlotter, QtCore.SIGNAL('valueChanged(int)'), lambda val: self.sbHandleValueChanged())
 
     def onLoad (self):
-        self.updateSlider()
+        self.sbUpdateRange()
 
     def onDataChanged (self, start, end):
         newRange = self.store.getRange()
         if newRange != self.currentRange:
-            self.updateSlider()
+            self.sbUpdateRange()
 
     def onZoom (self, factor):
         self.win.plotter.visibleSeconds /= float(factor)
-        self.updateSlider()
+        self.sbUpdateRange()
         self.win.plotter.update()
 
     def startTimer (self, usec, callback):
@@ -55,26 +171,27 @@ class QtOutput(BaseOutput):
         self.win.connect(timer, QtCore.SIGNAL('timeout()'), lambda: callback())
         timer.start(int(usec/1000.0))
 
-    def updateSlider (self):
+    def sbUpdateRange (self):
+        "new range and/or pageSize for scrollbar (or onLoad event)"
+
         (availStart, availEnd) = self.store.getRange()
         if availStart is None:
             return
 
-        stayAtEnd = (self.win.hscrollPlotter.value() == self.win.hscrollPlotter.maximum())
-
-        maxVal = (availEnd - self.win.plotter.visibleSeconds - availStart) * 1000
-        self.win.hscrollPlotter.setRange(0, maxVal)
-
-        self.win.hscrollPlotter.setPageStep(self.win.plotter.visibleSeconds * 1000)
-        self.win.hscrollPlotter.setSingleStep(1000)
+        stayAtEnd = self.hScroll.isAtMax()
+        self.hScroll.setRange(math.floor(availStart), math.ceil(availEnd - self.win.plotter.visibleSeconds),
+            self.win.plotter.visibleSeconds, self.win.plotter.visibleSeconds / 10.0)
+        self.currentRange = (availStart, availEnd)
 
         if stayAtEnd:
-            self.win.hscrollPlotter.setValue(self.win.hscrollPlotter.maximum())
+            self.hScroll.setValue(math.ceil(availEnd - self.win.plotter.visibleSeconds))
+            self.sbHandleValueChanged()
 
-        self.win.plotter.start = ((self.win.hscrollPlotter.value() / 1000.0) + availStart)
+    def sbHandleValueChanged (self):
+        "value has been changed"
+        self.win.plotter.start = self.hScroll.value()
         self.win.plotter.update()
 
-        self.currentRange = (availStart, availEnd)
 
     def run (self):
         # run onLoad method as soon as Qt mainloop has started
